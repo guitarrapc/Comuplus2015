4ea28784e564cce6e96af728ae187a610b27d8a8
guitarrapc <cherishacquire@outlook.com>
2015-09-26 11:18:25 +0900

    Add TopShelf Sample

---
+++ LightNodeSample/LightNodeSelfhost/Api/Tests.cs
@@ -0,0 +1,53 @@
+﻿using LightNode.Server;
+using LightNodeSelfHost.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LightNodeSelfHost.Api
+{
+    /// <summary>
+    /// Api のテストです
+    /// </summary>
+    public class Tests : LightNodeContract
+    {
+        /// <summary>
+        /// Hellow World を返します。
+        /// </summary>
+        /// <returns></returns>
+        [Get, Post]
+        public string ApiTest()
+        {
+            return "Hello World!";
+        }
+
+        /// <summary>
+        /// 実装されているAPIの一覧を返します。
+        /// </summary>
+        /// <remarks>APIのUriを返します。</remarks>
+        /// <returns></returns>
+        [Post]
+        public string[] ListApi()
+        {
+            var apis = LightNodeServerMiddleware.GetRegisteredHandlersInfo();
+            var key = apis.Select(x => x.Key).First();
+            return apis[key].SelectMany(x => x.RegisteredHandlers).Select(x => x.Key).ToArray();
+        }
+
+        /// <summary>
+        /// Hoge と Fuga の Echo を返します
+        /// </summary>
+        /// <returns></returns>
+        [Post]
+        public TestResponse Echo(string x, string y)
+        {
+            return new TestResponse
+            {
+                Hoge = x,
+                Fuga =y,
+            };
+        }
+    }
+}
---
+++ LightNodeSample/LightNodeSelfhost/DSCSample.ps1
@@ -0,0 +1,48 @@
+﻿configuration SampleTopShelfContinuousDelivery
+{
+    $s3Bucket = "INPUT YOUR S3 BUCKET NAME"
+
+    $zip = "S3 KEY ZIP FILE NAME"
+    $exe = "SERVICE EXE NAME"
+    $serviceName = "SERVICE NAME DEFINED IN TOPSHELF"
+
+    $destination = "C:\DOWNLOADPATH"
+    $downloadPath = Join-Path $destination $zip
+    $unzipPath = Join-Path $destination $serviceName
+    $servicePath = Join-Path $unzipPath "MSBUILD\EXTRACT\PATH\TO\$exe"
+
+    Import-DSCResource -ModuleName GraniResource
+
+    cS3Content SampleTopShelfContinuousDelivery
+    {
+        S3BucketName = $s3Bucket
+        Key = $zip
+        DestinationPath = $downloadPath
+        PreAction =  {
+            if(Get-Service | where Name -eq $using:serviceName){ Stop-Service $using:serviceName }
+            if(Test-Path $using:unzipPath){ Remove-Item -Path $using:unzipPath -Recurse -Force }
+        }
+    }
+ 
+    Archive SampleTopShelfContinuousDelivery
+    {
+        Ensure = 'Present'
+        Path = $downloadPath
+        Destination = $unzipPath
+    }
+
+    cTopShelf SampleTopShelfContinuousDelivery
+    {
+        Ensure = "Present"
+        Path = $servicePath
+        ServiceName = $serviceName
+        DependsOn = "[Archive]InstallRapidHouse"
+    }
+
+    Service SampleTopShelfContinuousDelivery
+    {
+        Name = $serviceName
+        State = 'Running'
+        DependsOn = "[cTopShelf]InstallRapidHouse"
+    }
+} 
